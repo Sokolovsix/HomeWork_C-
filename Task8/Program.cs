c814e95f2ab79da6d4c374f10779d1a641dbd8da
evgeny <sokolovsix@mail.ru>
2022-07-14 15:00:51 +0400

    add Task 8

---
+++ Task8/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+
+while(count <= N)
+{
+    if(count == N)
+    {
+      Console.WriteLine(count + " . ");
+    }
+    else
+    {
+        Console.WriteLine(count + " , ");
+    }
+    count = count + 2;
+}
