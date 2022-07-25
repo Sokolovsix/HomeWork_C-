edbf22bf8b655ccdd809a550afeb00289adea174
evgeny <sokolovsix@mail.ru>
2022-07-25 14:10:04 +0400

    add Task 15

---
+++ Task15/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введи число от 1 до 7: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+if (dayNumber == 6 || dayNumber == 7)
+{
+    Console.WriteLine("Да,выходной");
+}
+else
+{
+    Console.WriteLine("Не выходной");
+}
