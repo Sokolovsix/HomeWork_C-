0ce0a8a515ee88bcd9c95d1b1dd0eca7a6a370e6
evgeny <sokolovsix@mail.ru>
2022-07-25 13:22:56 +0400

    add Task 13

---
+++ Task13/Program.cs
@@ -0,0 +1,13 @@
+﻿int randomNumber = new Random().Next();
+
+string randomNumberText = Convert.ToString(randomNumber);
+
+if (randomNumberText.Length > 2)
+{
+    Console.WriteLine(randomNumber);
+    Console.WriteLine("Цифра" + randomNumberText[2]);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
